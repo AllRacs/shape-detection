89a27bac11557f34884edb6aa3a89fe91427d2b2
AllRacs <oscarplate@gmail.com>
2019-05-24 01:40:04 +0200

    añadida etiqueta que informacion, if boundgins

--- GerShapes.m
+++ GerShapes.m
@@ -1,12 +1,14 @@
-video = VideoReader('Sillas_1_2019_lenta.mp4');
+video = VideoReader('video\Sillas_1_2019_lenta.mp4');
 final = VideoWriter('GermyVideo.mp4', 'MPEG-4');
 open(final);
-for i=1:1:900
-   img = read(video, i);
-    imgGris = rgb2gray(img);
-    bw = im2bw(imgGris, [150/255]);
-    comp = imcomplement(bw);
-    cc = bwconncomp(comp);
+for i=1:3:2606
+    tic
+    img = read(video, i);
+%     imgGris = rgb2gray(img);
+%     bw = im2bw(imgGris, [150/255]);
+%     comp = imcomplement(bw);
+    [bw, bb] = rgbfunctionBlue(img);
+    cc = bwconncomp(bw);
     L = labelmatrix(cc);
     s = regionprops(L, 'Area', 'Perimeter');
     a = [s.Area];
@@ -18,9 +20,24 @@ for i=1:1:900
     L2 = labelmatrix(cc2);
     s2 = regionprops(L2, 'BoundingBox');
     bounding = [s2.BoundingBox];
-    triangulo = insertShape(img,'Rectangle', bounding, 'LineWidth', 5);
+    
+    si = size(bounding);
+    si = si(2);
+    nboun = si/4;
+    bounding2 = zeros(1,4);
+    if nboun > 1
+        bounding2 = bounding(si-3:si);
+    elseif nboun == 1
+        bounding2 = bounding;
+    end
+    
+    str = ['Perimetro: ' num2str(max(b)), fprintf('\n') 'Area: ' num2str(max(a))];
+
+    triangulo = insertObjectAnnotation(img, 'Rectangle', bounding2, str, 'FontSize', 20);
+    triangulo = insertShape(triangulo,'Rectangle', bounding2, 'LineWidth', 5);
+
     imshow(triangulo);
     writeVideo(final, triangulo);
+    toc
 end
 close(final);
-play(final);
