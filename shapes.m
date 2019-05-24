f84d513b9a5120456822351d662092089a86cce9
AllRacs <oscarplate@gmail.com>
2019-05-24 20:40:42 +0200

    final version

--- shapes.m
+++ shapes.m
@@ -1,26 +1,32 @@
 video = VideoReader('video\Sillas_1_2019_lenta.mp4');
-final = VideoWriter('GermyVideo.mp4', 'MPEG-4');
-%viewer = vision.DeployableVideoPlayer;
-
+final = VideoWriter('vprocesado.mp4', 'MPEG-4');
 open(final);
+video.CurrentTime = 80;
 while hasFrame(video)
     tic;
+    
     img = readFrame(video);
     [img1,img2] = createMask(img);
+    
     cc = bwconncomp(img1);
     L = labelmatrix(cc);
-    s = regionprops(L, 'Area', 'Perimeter', 'BoundingBox', 'Extent', 'Eccentricity');
+    s = regionprops(L, 'BoundingBox', 'Area', 'Extent', 'Eccentricity');
     a = [s.Area];
-    b = [s.Perimeter];
     ex = [s.Extent];
     ecc = [s.Eccentricity];
-    %triangularidad = (4*pi*a)./(b.*b);
+   
     idx = find(ex>0.3 & ex<0.6 & ecc<0.8 & ecc>0.2 & a>2000);
     bw2 = ismember(L, idx);
     cc2 = bwconncomp(bw2);
     L2 = labelmatrix(cc2);
-    s2 = regionprops(L2, 'BoundingBox');
+    s2 = regionprops(L2, 'BoundingBox', 'Area', 'Perimeter', 'Centroid');
+    
     bounding = [s2.BoundingBox];
+    a = [s2.Area];
+    b = [s2.Perimeter];
+    cen = [s2.Centroid];
+    cen(1,3) = 2;
+    scen = size(cen);
     
     si = size(bounding);
     si = si(2);
@@ -29,6 +35,7 @@ while hasFrame(video)
     bounding2 = zeros(1,4);
     if nboun > 1
         bounding2 = bounding(si-3:si);
+        cen = cen(scen(2)-1:scen);
     elseif nboun == 1
         bounding2 = bounding;
     end
@@ -36,29 +43,35 @@ while hasFrame(video)
     str = ['Perimetro: ' num2str(max(b)), fprintf('\n') 'Area: ' num2str(max(a))];
     triangulo = insertObjectAnnotation(img, 'Rectangle', bounding2, str, 'FontSize', 20);
     triangulo = insertShape(triangulo, 'Rectangle', bounding2, 'LineWidth', 5);
+    triangulo = insertShape(triangulo, 'Circle', cen, 'LineWidth', 5, 'color', 'red');
+    
     hImage = subplot(2, 2, 1);
     imshow(triangulo);
     title('Detección Triangulo RGB');
+    
     hImageB = subplot(2, 2, 2);
     imshow(img1);
     title('Detección Formas');
+    
     hImageB2 = subplot(2, 2, 3);
     imshow(bw2);
     title('Detección Triangulo BW');
     drawnow; % Refrescar.
     
-    imshow(triangulo);
     writeVideo(final, triangulo);
+    
     for i=1:3
         if hasFrame(video)
             aux = readFrame(video);
-            tri = insertObjectAnnotation(aux, 'Rectangle', bounding2, str);
+            tri = insertObjectAnnotation(aux, 'Rectangle', bounding2, str, 'FontSize', 20);
             tri = insertShape(tri, 'Rectangle', bounding2, 'LineWidth', 5);
+            tri = insertShape(tri, 'Circle', cen, 'LineWidth', 5, 'color', 'red');
+            
             writeVideo(final, tri);
         end
     end
+    
     toc
 end
-close(final);
-implay(final);
 
+close(final);
